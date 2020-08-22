db33730cf01ce36892e0bb6e57ad58b21dccd1f3
joypaul <jpaul3250@gmail.com>
2020-08-22 00:16:49 -0400

    resume update

--- lib/home_screen/models/social_urls.dart
+++ lib/home_screen/models/social_urls.dart
@@ -2,5 +2,5 @@ class Urls {
   final githubUrl = "https://github.com/cosmicRover";
   final linkedInUrl = "https://www.linkedin.com/in/joy-paul-050586b1";
   final resumeUrl =
-      "https://drive.google.com/file/d/1lvVBNYh4f5sbnOoDIMsZ1dfBVPmcY6mq/view?usp=sharing";
+      "https://drive.google.com/file/d/1GkUV4zmMMc13YLmA_ttyCX3npLcnw3Xc/view?usp=sharing";
 }
